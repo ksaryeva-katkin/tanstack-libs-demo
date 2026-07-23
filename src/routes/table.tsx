15db37f5fa27e2726625bb12277402495acbaab5
Klara <klara.saryeva@katkin.com>
2026-07-23 15:37:44 +0600

    user name check

--- src/routes/table.tsx
+++ src/routes/table.tsx
@@ -9,7 +9,7 @@ function TablePage() {
     <section>
       <h2 className="text-xl font-semibold text-white">Table</h2>
       <p className="mt-2 text-sm text-zinc-400">
-        TODO: реализовать табличное представление в следующем этапе.
+        TODO: реализовать табличное представление в следующем этапе
       </p>
     </section>
   );
